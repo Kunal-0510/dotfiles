b4c6742278baffdc81b3d29cec128293c97ea3f9
kunal <kunalcode05@gmail.com>
2025-06-19 14:05:56 +0530

    fixed blink crash

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -6,6 +6,7 @@ return {
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
     opts = {
+        render_modes = {'n','c','t'},
         latex = {
             enabled = true,
             live_update = true,
